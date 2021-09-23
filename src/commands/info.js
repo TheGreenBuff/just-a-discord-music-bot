0fcc1445ab0cb8907269bc5dbb11cd95d573f37e
TheGreenBuff <50930416+TheGreenBuff@users.noreply.github.com>
2021-09-23 14:57:40 +0200

    Create info.js

---
+++ src/commands/info.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'info',
+	description: 'Skip command.',
+	cooldown: 5,
+	execute(message, Discord) {
+		const newEmbed = new Discord.MessageEmbed()
+		.setColor()
+		.setTitle('Info')
+		.setDescription('Music bot AutismHQ!')
+		.addFields(
+			{name: '!info', value: 'This command'},
+			{name: '!play', value: 'url or name of song'},
+			{name: '!np', value: 'shows current song playing'},
+			{name: '!skip', value: 'skips current song'},
+			{name: '!q or !queue', value: 'shows queue'},
+			{name: '!pause', value: 'pauses song'},
+			{name: '!resume', value: 'resumes song'},
+			{name: '!volume', value: 'changes general volume'},
+			{name: '!stop', value: 'Disconnects me'}
+		.setFooter(':)')
+	}
+};
